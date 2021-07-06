8f55b028366f35d26a1b61fe8aa5acaf66978cb6
Riley Lum <rileylum@adam.com.au>
2021-07-06 18:22:02 +0930

    add create timeblock functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,32 @@ function setCurrentDay() {
 }
 
 setCurrentDay();
+
+function createTimeBlock(hour) {
+    var timeBlockRow = document.createElement("div");
+    timeBlockRow.classList.add("row", "time-block");
+
+    var timeBlockTime = document.createElement("div");
+    timeBlockTime.classList.add("col-2", "hour");
+    timeBlockTime.textContent = moment(hour, "HH").format("hA");
+    var timeBlockDesc = document.createElement("textarea");
+    timeBlockDesc.classList.add("col-8", "description", "present");
+    var timeBlockSave = document.createElement("button");
+    timeBlockSave.classList.add("col-2", "saveBtn");
+    var saveIcon = document.createElement("i");
+    saveIcon.classList.add("fa", "fa-save", "icon-size");
+
+    timeBlockSave.appendChild(saveIcon);
+
+    timeBlockRow.appendChild(timeBlockTime);
+    timeBlockRow.appendChild(timeBlockDesc);
+    timeBlockRow.appendChild(timeBlockSave);
+
+    var timeBlockSection = document.querySelector(".container");
+
+    timeBlockSection.appendChild(timeBlockRow);
+}
+
+for (var i = 9; i < 18; i++) {
+    createTimeBlock(i);
+}
