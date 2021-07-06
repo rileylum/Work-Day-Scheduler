c5b00f9f7300329165913ec3caf98d49803aa4c8
Riley Lum <rileylum@adam.com.au>
2021-07-06 21:18:55 +0930

    add functionality for local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+var timeBlockSection = document.querySelector(".container");
+
 
 // get current day from momentJS and display it on webpage
 function setCurrentDay() {
@@ -13,12 +15,14 @@ setCurrentDay();
 function createTimeBlock(hour) {
     var timeBlockRow = document.createElement("div");
     timeBlockRow.classList.add("row", "time-block");
+    timeBlockRow.setAttribute("data-index", hour - 9);
 
     var timeBlockTime = document.createElement("div");
     timeBlockTime.classList.add("col-2", "hour");
     timeBlockTime.textContent = moment(hour, "HH").format("hA");
     var timeBlockDesc = document.createElement("textarea");
     timeBlockDesc.classList.add("col-8", "description", "present");
+    timeBlockDesc.value = scheduleArr[hour - 9];
     var timeBlockSave = document.createElement("button");
     timeBlockSave.classList.add("col-2", "saveBtn");
     var saveIcon = document.createElement("i");
@@ -30,10 +34,33 @@ function createTimeBlock(hour) {
     timeBlockRow.appendChild(timeBlockDesc);
     timeBlockRow.appendChild(timeBlockSave);
 
-    var timeBlockSection = document.querySelector(".container");
+
 
     timeBlockSection.appendChild(timeBlockRow);
 }
+
+
+
+var scheduleArr;
+var storedScheduleArr = JSON.parse(localStorage.getItem('schedule'));
+
+if (!storedScheduleArr) {
+    // empty array of length for each hour in work day
+    scheduleArr = [, , , , , , , ,];
+} else {
+    scheduleArr = storedScheduleArr;
+};
+
+timeBlockSection.addEventListener('click', function (e) {
+    console.log(e.target);
+    if (e.target.tagName === 'BUTTON') {
+        scheduleArr[e.target.parentElement.getAttribute('data-index')] = e.target.parentElement.children[1].value;
+        localStorage.setItem('schedule', JSON.stringify(scheduleArr));
+
+    } else {
+        console.log("not button");
+    };
+})
 
 for (var i = 9; i < 18; i++) {
     createTimeBlock(i);
